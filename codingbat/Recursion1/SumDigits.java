a7e11c03b8d92a3b4cd95924a097c65340ec3835
bartek <bartlomiejgmerek@outlook.com>
2020-05-27 20:48:41 +0200

    recursion next

--- codingbat/Recursion1/SumDigits.java
+++ codingbat/Recursion1/SumDigits.java
@@ -2,7 +2,8 @@ package com.codingbat.Recursion1;
 
 public class SumDigits {
     /*
-    Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    Given a non-negative int n, return the sum of its digits recursively (no loops).
+    Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
 sumDigits(126) → 9
 sumDigits(49) → 13
