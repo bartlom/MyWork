128ad94f8a5966d205b4fde73de02345b21a0903
bartlom <65561690+bartlom@users.noreply.github.com>
2020-06-07 16:56:30 +0200

    Update MaxMirror.java

--- codingbat/Array3/MaxMirror.java
+++ codingbat/Array3/MaxMirror.java
@@ -3,9 +3,10 @@ package com.codingbat.array3;
 public class MaxMirror {
 
     /*
-    We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array, the same group appears in reverse order.
-    For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
-    Return the size of the largest mirror section found in the given array.
+We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array, 
+the same group appears in reverse order.
+For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+Return the size of the largest mirror section found in the given array.
 
 maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
 maxMirror([1, 2, 1, 4]) → 3
@@ -26,10 +27,7 @@ maxMirror([5, 9, 9, 4, 5, 4, 9, 9, 2]) → 7
 maxMirror([5, 9, 9, 6, 5, 4, 9, 9, 2]) → 2
 maxMirror([5, 9, 1, 6, 5, 4, 1, 9, 5]) → 3
 
-
-
-
-     */
+*/
     public static int[] numsMirror = new int[]{5, 9, 1, 6, 5, 4, 1, 9, 5};
 
     public static int maxMirror(int[] nums) {
