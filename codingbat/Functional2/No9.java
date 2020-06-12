2decfdeeff8be62a3c8a3ecc10606b7a72e5bebd
bartek <bartlomiejgmerek@outlook.com>
2020-06-12 23:30:57 +0200

    correct

--- codingbat/Functional2/No9.java
+++ codingbat/Functional2/No9.java
@@ -1,4 +1,33 @@
 package com.codingbat.Functional2;
 
+/*
+
+Given a list of non-negative integers, return a list of those
+numbers except omitting any that end with 9. (Note: % by 10)
+
+no9([1, 2, 19]) → [1, 2]
+no9([9, 19, 29, 3]) → [3]
+no9([1, 2, 3]) → [1, 2, 3]
+no9([45, 99, 90, 28, 13, 999, 0]) → [45, 90, 28, 13, 0]
+no9([]) → []
+no9([9]) → []
+no9([0, 9, 0]) → [0, 0]
+
+ */
+
+import java.util.List;
+
 public class No9 {
+
+    public List<Integer> no9(List<Integer> nums) {
+        nums.removeIf(n -> rightmostDigits10(n) == 9);
+        return nums;
+    }
+
+    public Integer rightmostDigits10(int num) {
+        for (int i = 1; i < String.valueOf(num).length(); i++) {
+            num = num % 10;
+        }
+        return num;
+    }
 }
--- codingbat/Functional2/NoTeen.java
+++ codingbat/Functional2/NoTeen.java
@@ -1,4 +1,26 @@
 package com.codingbat.Functional2;
 
+/*
+
+Given a list of integers, return a list of those numbers,
+omitting any that are between 13 and 19 inclusive.
+
+noTeen([12, 13, 19, 20]) → [12, 20]
+noTeen([1, 14, 1]) → [1, 1]
+noTeen([15]) → []
+noTeen([-15]) → [-15]
+noTeen([]) → []
+noTeen([0, 1, 2, -3]) → [0, 1, 2, -3]
+noTeen([15, 17, 19, 21, 19]) → [21]
+noTeen([-16, 2, 15, 3, 16, 25]) → [-16, 2, 3, 25]
+
+ */
+
+import java.util.List;
+
 public class NoTeen {
+    public List<Integer> noTeen(List<Integer> nums) {
+        nums.removeIf(n -> n >= 13 && n <= 19);
+        return nums;
+    }
 }
